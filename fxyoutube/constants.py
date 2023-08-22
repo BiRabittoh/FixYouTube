83b48d6e202262d67446f68e0edf5357fcf215a4
Marco Andronaco <andronacomarco@gmail.com>
2023-08-22 09:46:07 +0200

    update repo url

--- fxyoutube/constants.py
+++ fxyoutube/constants.py
@@ -4,6 +4,6 @@ DB_URL = "cache.db"
 
 UA_REGEX = r"bot|facebook|embed|got|firefox\/92|firefox\/38|curl|wget|go-http|yahoo|generator|whatsapp|preview|link|proxy|vkshare|images|analyzer|index|crawl|spider|python|cfnetwork|node"
 BASE_URL = "https://www.youtube.com/watch?v="
-REPO_URL = "https://github.com/BiRabittoh/fxyoutube"
+REPO_URL = "https://github.com/BiRabittoh/FixYouTube"
 TS_FORMAT = "%Y-%m-%d %H:%M:%S"
 MAX_SIZE_BYTES = MAX_SIZE_MB * 1_000_000
