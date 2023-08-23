dc439938a19e7440a7e26c8fd9481610c51d6a5a
Marco Andronaco <andronacomarco@gmail.com>
2023-08-23 14:52:52 +0200

    better filtering, add thumbnail

--- fxyoutube/constants.py
+++ fxyoutube/constants.py
@@ -5,6 +5,7 @@ DB_URL = "cache.db"
 UA_REGEX = r"bot|facebook|embed|got|firefox\/92|firefox\/38|curl|wget|go-http|yahoo|generator|whatsapp|preview|link|proxy|vkshare|images|analyzer|index|crawl|spider|python|cfnetwork|node"
 BASE_URL = "https://www.youtube.com/watch?v="
 REPO_URL = "https://github.com/BiRabittoh/FixYouTube"
-TS_FORMAT = "%Y-%m-%d %H:%M:%S"
 PROXY_HEADERS = { "Content-Type": "video/mp4" }
-YTDL_OPTS = { "format": f"best[ext=mp4][filesize<{ MAX_SIZE_MB }M][protocol^=http][protocol!*=dash]" }
+YTDL_OPTS = { "format": f"best[ext=mp4][filesize<{ MAX_SIZE_MB }M][protocol^=http][protocol!*=dash] / (bv*+ba/b)" }
+YTDL_KEYS = [ "id", "title", "description", "uploader", "duration", "height", "width", "url" ]
+URL_KEY = YTDL_KEYS[-1]
--- fxyoutube/yt_info.py
+++ fxyoutube/yt_info.py
@@ -1,8 +1,6 @@
 from yt_dlp import YoutubeDL, DownloadError
 import fxyoutube.constants as c
 
-ydl_keys = ["id", "title", "description", "uploader", "duration", "height", "width", "url"]
-
 def truncate_lines(input_str: str, max: int = 4):
     return "\n".join(input_str.splitlines()[:max])
 
@@ -10,13 +8,10 @@ def get_info_ytdl(yt_id: str):
     try:
         with YoutubeDL(c.YTDL_OPTS) as ydl:
             info = ydl.extract_info(c.BASE_URL + yt_id, download=False)
-            yt_info = { k: info[k] for k in ydl_keys }
+            yt_info = { k: info[k] for k in c.YTDL_KEYS[:-1] }
+            yt_info[c.URL_KEY] = info.get(c.URL_KEY, "")
     except DownloadError:
-        with YoutubeDL() as ydl:
-            info = ydl.extract_info(c.BASE_URL + yt_id, download=False)
-            yt_info = { k: info[k] for k in ydl_keys[:-3] }
-            yt_info["height"] = yt_info["width"] = 0
-            yt_info["url"] = ""
-            
+        return None
+    
     yt_info["description"] = truncate_lines(yt_info["description"])
     return yt_info
