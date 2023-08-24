556aabf6284d0dc94693887934a3c04a3353c1ce
Marco Andronaco <andronacomarco@gmail.com>
2023-08-24 15:17:26 +0200

    galactic bugfix

--- fxyoutube/constants.py
+++ fxyoutube/constants.py
@@ -6,6 +6,6 @@ UA_REGEX = r"bot|facebook|embed|got|firefox\/92|firefox\/38|curl|wget|go-http|ya
 BASE_URL = "https://www.youtube.com/watch?v="
 REPO_URL = "https://github.com/BiRabittoh/FixYouTube"
 PROXY_HEADERS = { "Content-Type": "video/mp4" }
-YTDL_OPTS = { "format": f"best[ext=mp4][filesize<{ MAX_SIZE_MB }M][protocol^=http][protocol!*=dash] / (bv*+ba/b)" }
+YTDL_OPTS = { "format": f"best[ext=mp4][filesize<?{ MAX_SIZE_MB }M][filesize_approx<?{ MAX_SIZE_MB }M][protocol^=http][protocol!*=dash] / (bv*+ba/b)" }
 YTDL_KEYS = [ "id", "title", "description", "uploader", "duration", "height", "width", "url" ]
 URL_KEY = YTDL_KEYS[-1]
